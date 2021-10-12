d0e4c192a37879f2cd0e1f31b624bece5416daaf
NicholasBoss <nicholasboss0914@gmail.com>
2021-10-12 15:57:47 -0600

    Here's the Display!

---
+++ 05-jumper/Display.cs
@@ -0,0 +1,22 @@
+namespace _05_jumper
+{
+    class Display
+    {
+       public void RemoveLine()
+       {
+           
+       }
+       public void AddLetter()
+       {
+           
+       }
+       public void DisplayWord()
+       {
+           
+       }
+       public void createParachute()
+       {
+
+       }
+    }
+}
