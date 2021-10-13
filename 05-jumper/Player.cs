0395623d8205149d2c1dcce914987555b92f1c4f
NicholasBoss <nicholasboss0914@gmail.com>
2021-10-13 13:26:09 -0600

    Newest changes from class today

--- 05-jumper/Player.cs
+++ 05-jumper/Player.cs
@@ -1,10 +1,12 @@
+using System;
 namespace _05_jumper
 {
     class Player
     {
        public void Input()
        {
-           
+           Console.WriteLine("Guess a letter [a-z]:");
+            string read = Console.ReadLine();
        }
     }
 }
