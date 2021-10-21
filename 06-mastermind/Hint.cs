0a59175d45ffbbac8608f4fb7d0e2d6b37a773db
SilverKnight317 <ajclarkson3@gmail.com>
2021-10-21 14:17:02 -0600

    refined Hint.cs for when we start referencing

--- 06-mastermind/Hint.cs
+++ 06-mastermind/Hint.cs
@@ -18,13 +18,15 @@ namespace _06_mastermind
 
         /// <summary>
         /// The ProcessHint() function throws out a hint for the current guess to the code.
-        /// It does this through using one Stringbuilder Variable, 
+        /// <summary>
         private void ProcessHint(string originalCode, string guessedCode)
         {
             StringBuilder sb = new StringBuilder();
             List<string> guessedWord = new List<string>();
             List<string> actualCode = new List<string>();
             List<string> hint = new List<string>();
+            
+            // the 2 foreach loops are made to build the lists for the generated code and the guessed code
             foreach(char letter in guessedCode)
             {
                 guessedWord.Add(letter.ToString());
@@ -33,6 +35,8 @@ namespace _06_mastermind
             {
                 actualCode.Add(letter.ToString());
             }
+
+            // The for loop in a for loop is to check for potential numbers that are generally guessed correctly ("o") and then if a number was exactly correct ("x").
             for (int u = 0; u <= 4; u++)
             {
                 actualCode[u] = "*";
@@ -48,10 +52,14 @@ namespace _06_mastermind
                     hint[u] = "x";
                 }
             }
+
+            // this for loop is to build the hint within the StringBuilder
             for (int a = 0; a <= 4; a++)
             {
                 sb.Append(hint[a]);
             }
+            // after the for loop return the Stringbuilder that's converted into a string using the .ToString() function.
+
             // return sb.ToString();
         }
     }
